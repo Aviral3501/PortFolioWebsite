5b75d68be0ba81425430a1d19a76e4c1be7036f5
Aviral Singh <aviralsingh3501@gmail.com>
2024-07-20 19:32:16 +0530

    image removed

--- frontend/src/container/Header/Header.jsx
+++ frontend/src/container/Header/Header.jsx
@@ -46,17 +46,17 @@ const Header = () => {
       </motion.div>
 
 
-      <motion.div
+      {/* <motion.div
        whileInView={{opacity:[0,1]}} 
        transition={{duration: 0.5 , delayChildren:0.5 }}  //how long transition takes
        className='app__header-img'>
-        <img src={images.profile} alt='profile_bg'/>
+        <img src={images.profile} alt='profile_bg'/> 
         <motion.img
         whileInView={{scale:[0,1]}}
         transition={{duration:1, ease:"easeInOut"}}
        
         src={images.circle} alt='profile_circle' className='overlay_circle' />
-       </motion.div>
+       </motion.div> */}
 
 
       <motion.div
